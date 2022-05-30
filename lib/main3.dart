e15701050f6e3708185ed50e97407e598532978a
shaymaa <nassarelshaymaa@gmail.com>
2022-05-30 11:03:51 +0200

    now done

---
+++ lib/main3.dart
@@ -0,0 +1,105 @@
+import 'dart:convert';
+// import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'OfferDataModel1.dart';
+import 'package:flutter/services.dart' as rootBundle;
+import 'dart:async';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: FutureBuilder(
+      future: ReadJsonData(),
+      builder: (context, data) {
+        if (data.hasError) {
+          return Center(child: Text("${data.error}"));
+        } else if (data.hasData) {
+          var items = data.data as List<OfferDataModel1>;
+          return ListView.builder(
+              itemCount: items == null ? 0 : items.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  margin: EdgeInsets.symmetric(horizontal: 10, vertical: 6),
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Container(
+                            width: 50,
+                            height: 50,
+                            child: Image(
+                              image: AssetImage(items[index].photo.toString()),
+                              height: 100.0,
+                              width: 100.0,
+                            )),
+                        Expanded(
+                            child: Container(
+                          padding: EdgeInsets.only(bottom: 8),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(
+                                  items[index].directory.toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                              Padding(
+                                padding: EdgeInsets.only(left: 8, right: 8),
+                                child: Text(items[index].id.toString()),
+                              )
+                            ],
+                          ),
+                        ))
+                      ],
+                    ),
+                  ),
+                );
+              });
+        } else {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+      },
+    ));
+  }
+
+  Future<List<OfferDataModel1>> ReadJsonData() async {
+    final jsondata =
+        await rootBundle.rootBundle.loadString('jsonfile/offers1.json');
+    final list = json.decode(jsondata) as List<dynamic>;
+    return list.map((e) => OfferDataModel1.fromJson(e)).toList();
+  }
+}
